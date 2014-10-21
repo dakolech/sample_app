4aa63380f3b6611b1e2fea51e02d4b00b11a3b6c
Daniel <dakolech@gmail.com>
2014-10-21 17:31:50 +0200

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ end
 source 'https://rubygems.org'
 
 gem 'rails',                '4.2.0.beta2'
+gem 'bootstrap-sass',       '3.2.0.0'
 gem 'sass-rails',           '5.0.0.beta1'
 gem 'uglifier',             '2.5.3'
 gem 'coffee-rails',         '4.0.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  root             'static_pages#home'
 
-  get 'static_pages/about'
+  get 'help'    => 'static_pages#help'
+
+  get 'about'   => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
