148f4c47d356aa6d770d44bf001b6bbbf8e0a1cb
Daniel <dakolech@gmail.com>
2014-10-24 19:52:46 +0200

    half sign up

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'signup'  => 'users#new'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
