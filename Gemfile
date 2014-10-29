baa4e8769d749b49d30e57dc7c4e633407d11119
Daniel <dakolech@gmail.com>
2014-10-29 22:40:21 +0100

    users index

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ ruby   '2.0.0'
 
 gem 'rails',                '4.2.0.beta2'
 gem 'bcrypt',               '3.1.7'
+gem 'faker',                '1.4.2'
 gem 'bootstrap-sass',       '3.2.0.0'
 gem 'sass-rails',           '5.0.0.beta1'
 gem 'uglifier',             '2.5.3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -47,8 +52,14 @@ class UsersController < ApplicationController
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
