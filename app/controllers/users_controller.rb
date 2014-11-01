037b551facdb6434f0f7976d958182fecad164c8
Daniel <dakolech@gmail.com>
2014-11-01 18:16:12 +0100

    start with microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
